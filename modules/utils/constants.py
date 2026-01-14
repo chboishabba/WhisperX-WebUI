81567380b275b03e422006ea89114220935e6c3d
Johl Brown <johlbrown@gmail.com>
2026-01-14 16:43:15 +1000

    Avoid gettext before Gradio request

--- modules/utils/constants.py
+++ modules/utils/constants.py
@@ -1,6 +1,9 @@
 from gradio_i18n import Translate, gettext as _
 
-AUTOMATIC_DETECTION = _("Automatic Detection")
+try:
+    AUTOMATIC_DETECTION = _("Automatic Detection")
+except LookupError:
+    AUTOMATIC_DETECTION = "Automatic Detection"
 GRADIO_NONE_STR = ""
 GRADIO_NONE_NUMBER_MAX = 9999
 GRADIO_NONE_NUMBER_MIN = 0
